3c825efe73b13405c1db6ebe6aa759629389c9b6
Stephen Childs <sechilds@gmail.com>
2010-12-07 13:14:16 -0500

    Adding more variables.
    	modified:   ruby/parselabels2.rb

--- ruby/parselabels2.rb
+++ ruby/parselabels2.rb
@@ -66,3 +66,39 @@ f2.close
 f3.close
 f4.close
 f5.close
+
+f1 = File.open("/Users/sechilds/uoirp/data/user/labels/imstat_codes.do", "w")
+f2 = File.open("/Users/sechilds/uoirp/data/user/labels/imstat_en_label.do", "w")
+f3 = File.open("/Users/sechilds/uoirp/data/user/labels/imstat_fr_label.do", "w")
+mydata = FasterCSV.read('/Users/sechilds/uoirp/data/user/labels/imstat.txt',:headers => :first_row,:col_sep =>"\t") 
+mydata.each_with_index do |row, index|
+  f1.puts("label define imstat_codes " + (index+1).to_s + " " + '"' + row[0].to_s.strip + '"' + ", add")
+  f2.puts("label define imstat_en_label " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+  f3.puts("label define imstat_fr_label " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+end
+
+f1.puts("")
+f2.puts("")
+f3.puts("")
+f1.close
+f2.close
+f3.close
+
+f1 = File.open("/Users/sechilds/uoirp/data/user/labels/kind_of_program_cd_codes.do", "w")
+f2 = File.open("/Users/sechilds/uoirp/data/user/labels/kind_of_program_cd_en_label.do", "w")
+f3 = File.open("/Users/sechilds/uoirp/data/user/labels/kind_of_program_cd_fr_label.do", "w")
+mydata = FasterCSV.read('/Users/sechilds/uoirp/data/user/labels/kind_of_program_cd.txt',:headers => :first_row,:col_sep =>"\t") 
+mydata.each_with_index do |row, index|
+  f1.puts("label define kind_of_program_cd_codes " + (index+1).to_s + " " + '"' + row[0].to_s.strip + '"' + ", add")
+  f2.puts("label define kind_of_program_cd_en_label " + (index+1).to_s + " " + '"' + row[2].to_s.strip + '"' + ", add")
+  f3.puts("label define kind_of_program_cd_fr_label " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+end
+
+f1.puts("")
+f2.puts("")
+f3.puts("")
+f1.close
+f2.close
+f3.close
+
+
