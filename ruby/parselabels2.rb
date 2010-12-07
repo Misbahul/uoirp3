2fdf755d3ee8d7d4dbd64cf3434fd0a65b100d78
Stephen Childs <sechilds@gmail.com>
2010-12-07 12:44:56 -0500

    Added first attempt at county code processing.
    * The county codes are stored as numbers already. This
    makes things a bit simpler.
    
    	modified:   ruby/parselabels2.rb

--- ruby/parselabels2.rb
+++ ruby/parselabels2.rb
@@ -21,4 +21,21 @@ f1.close
 f2.close
 f3.close
 
+# f1 = File.open("/Users/sechilds/uoirp/data/user/labels/county_codes.do", "w")
+f2 = File.open("/Users/sechilds/uoirp/data/user/labels/county_en_label.do", "w")
+f3 = File.open("/Users/sechilds/uoirp/data/user/labels/county_fr_label.do", "w")
+mydata = FasterCSV.read('/Users/sechilds/uoirp/data/user/labels/county.txt',:headers => :first_row,:col_sep =>"\t") 
+mydata.each do |row|
+  # f1.puts("label define apptype_codes " + (index+1).to_s + " " + '"' + row[0].to_s.strip + '"' + ", add")
+  f2.puts("label define apptype_en_label " + row[1].to_s + " " + '"' + row[2].to_s.strip + '"' + ", add")
+  f3.puts("label define apptype_fr_label " + row[1].to_s + " " + '"' + row[3].to_s.strip + '"' + ", add")
+end
+
+# f1.puts("")
+f2.puts("")
+f3.puts("")
+# f1.close
+f2.close
+f3.close
+
 
