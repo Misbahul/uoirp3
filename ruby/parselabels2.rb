b656abff6c29b6882298e94dfb0bd88466b70a5e
Stephen Childs <sechilds@gmail.com>
2010-12-07 13:10:07 -0500

    Added code to process credential type.
    	modified:   ruby/parselabels2.rb

--- ruby/parselabels2.rb
+++ ruby/parselabels2.rb
@@ -42,4 +42,27 @@ f3.puts("")
 f2.close
 f3.close
 
+f1 = File.open("/Users/sechilds/uoirp/data/user/labels/credential_cd_codes.do", "w")
+f2 = File.open("/Users/sechilds/uoirp/data/user/labels/credential_cd_en_label.do", "w")
+f3 = File.open("/Users/sechilds/uoirp/data/user/labels/credential_cd_fr_label.do", "w")
+f4 = File.open("/Users/sechilds/uoirp/data/user/labels/credential_cd_en_abbrev.do", "w")
+f5 = File.open("/Users/sechilds/uoirp/data/user/labels/credential_cd_fr_abbrev.do", "w")
+mydata = FasterCSV.read('/Users/sechilds/uoirp/data/user/labels/credential_cd.txt',:headers => :first_row,:col_sep =>"\t") 
+mydata.each_with_index do |row, index|
+  f1.puts("label define credential_cd_codes " + (index+1).to_s + " " + '"' + row[0].to_s.strip + '"' + ", add")
+  f2.puts("label define credential_cd_en_label " + (index+1).to_s + " " + '"' + row[2].to_s.strip + '"' + ", add")
+  f3.puts("label define credential_cd_fr_label " + (index+1).to_s + " " + '"' + row[3].to_s.strip + '"' + ", add")
+  f4.puts("label define credential_cd_en_abbrev " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+  f5.puts("label define credential_cd_fr_abbrev " + (index+1).to_s + " " + '"' + row[5].to_s.strip + '"' + ", add")
+end
 
+f1.puts("")
+f2.puts("")
+f3.puts("")
+f4.puts("")
+f5.puts("")
+f1.close
+f2.close
+f3.close
+f4.close
+f5.close
