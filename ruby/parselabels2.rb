3e245d75808b84ed71b4dd2c6d65a3b74179d712
Stephen Childs <sechilds@gmail.com>
2010-12-07 13:20:51 -0500

    My ruby program now handles all the variables.
    Added mother tongue, principle teaching language, subject code,
    and ug_spec_level as well.
    
    	modified:   ruby/parselabels2.rb

--- ruby/parselabels2.rb
+++ ruby/parselabels2.rb
@@ -101,4 +101,72 @@ f1.close
 f2.close
 f3.close
 
+f1 = File.open("/Users/sechilds/uoirp/data/user/labels/mother_tongue_codes.do", "w")
+f2 = File.open("/Users/sechilds/uoirp/data/user/labels/mother_tongue_en_label.do", "w")
+f3 = File.open("/Users/sechilds/uoirp/data/user/labels/mother_tongue_fr_label.do", "w")
+mydata = FasterCSV.read('/Users/sechilds/uoirp/data/user/labels/mother_tongue.txt',:headers => :first_row,:col_sep =>"\t") 
+mydata.each_with_index do |row, index|
+  f1.puts("label define mother_tongue_codes " + (index+1).to_s + " " + '"' + row[0].to_s.strip + '"' + ", add")
+  f2.puts("label define mother_tongue_en_label " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+  f3.puts("label define mother_tongue_fr_label " + (index+1).to_s + " " + '"' + row[2].to_s.strip + '"' + ", add")
+end
+
+f1.puts("")
+f2.puts("")
+f3.puts("")
+f1.close
+f2.close
+f3.close
+
+f1 = File.open("/Users/sechilds/uoirp/data/user/labels/princ_teaching_lng_codes.do", "w")
+f2 = File.open("/Users/sechilds/uoirp/data/user/labels/princ_teaching_lng_en_label.do", "w")
+f3 = File.open("/Users/sechilds/uoirp/data/user/labels/princ_teaching_lng_fr_label.do", "w")
+mydata = FasterCSV.read('/Users/sechilds/uoirp/data/user/labels/princ_teaching_lng.txt',:headers => :first_row,:col_sep =>"\t") 
+mydata.each_with_index do |row, index|
+  f1.puts("label define princ_teaching_lng_codes " + (index+1).to_s + " " + '"' + row[0].to_s.strip + '"' + ", add")
+  f2.puts("label define princ_teaching_lng_en_label " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+  f3.puts("label define princ_teaching_lng_fr_label " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+end
+
+f1.puts("")
+f2.puts("")
+f3.puts("")
+f1.close
+f2.close
+f3.close
+
+
+f1 = File.open("/Users/sechilds/uoirp/data/user/labels/subject_cd_codes.do", "w")
+f2 = File.open("/Users/sechilds/uoirp/data/user/labels/subject_cd_en_label.do", "w")
+f3 = File.open("/Users/sechilds/uoirp/data/user/labels/subject_cd_fr_label.do", "w")
+mydata = FasterCSV.read('/Users/sechilds/uoirp/data/user/labels/subject_cd.txt',:headers => :first_row,:col_sep =>"\t") 
+mydata.each_with_index do |row, index|
+  f1.puts("label define subject_cd_codes " + (index+1).to_s + " " + '"' + row[0].to_s.strip + '"' + ", add")
+  f2.puts("label define subject_cd_en_label " + (index+1).to_s + " " + '"' + row[2].to_s.strip + '"' + ", add")
+  f3.puts("label define subject_cd_fr_label " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+end
+
+f1.puts("")
+f2.puts("")
+f3.puts("")
+f1.close
+f2.close
+f3.close
+
+f1 = File.open("/Users/sechilds/uoirp/data/user/labels/ug_spec_level_cd_codes.do", "w")
+f2 = File.open("/Users/sechilds/uoirp/data/user/labels/ug_spec_level_cd_en_label.do", "w")
+f3 = File.open("/Users/sechilds/uoirp/data/user/labels/ug_spec_level_cd_fr_label.do", "w")
+mydata = FasterCSV.read('/Users/sechilds/uoirp/data/user/labels/ug_spec_level_cd.txt',:headers => :first_row,:col_sep =>"\t") 
+mydata.each_with_index do |row, index|
+  f1.puts("label define ug_spec_level_cd_codes " + (index+1).to_s + " " + '"' + row[0].to_s.strip + '"' + ", add")
+  f2.puts("label define ug_spec_level_cd_en_label " + (index+1).to_s + " " + '"' + row[2].to_s.strip + '"' + ", add")
+  f3.puts("label define ug_spec_level_cd_fr_label " + (index+1).to_s + " " + '"' + row[1].to_s.strip + '"' + ", add")
+end
+
+f1.puts("")
+f2.puts("")
+f3.puts("")
+f1.close
+f2.close
+f3.close
 
